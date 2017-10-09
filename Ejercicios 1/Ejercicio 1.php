2c38eefa9978dd8b34ab5d222767c824641be82b
UrielKaralden <urielkaralden@gmail.com>
2017-10-09 13:14:55 +0200

    "First attempts with PHP"

---
+++ Ejercicios 1/Ejercicio 1.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title="Ejercicio 1"> </title>
+</head>
+
+<body>
+    <h1> Concatenacion de cadenas </h1>
+
+<?php
+    $pw = 'Programación Web';
+    $s1 = "Bienvenid@s";
+    $s2 = " a $pw";
+    echo $s1.$s2;
+ ?>
+</body>
+</html>
---
+++ Ejercicios 1/Ejercicio 2.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <title="Ejercicio 2"> </title>
+</head>
+
+<body>
+    <h1> Suma de variables </h1>
+
+<?php
+    $string = 'El resultado es ';
+    $n1 = 10;
+    $n2 = 31;
+    $res = $n1 + $n2;
+
+    echo $string.$res <br> \n ;
+ ?>
+</body>
+</html>
