d654eade39ef017505c244eea39d48642d58be21
UrielKaralden <urielkaralden@gmail.com>
2017-10-11 21:28:32 +0200

    Vectors with PHP

---
+++ Ejercicios 1/Ejercicio 7.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title="Ejercicio 7"> </title>
+    </head>
+
+    <body>
+        <h1> Potencias </h1>
+        <!--<div align=center>  Alineamiento central en HTML -->
+            <?php
+
+            $res = 1;
+            for($i = 0; $i < 10; ++$i)
+            {
+                $vector[$i] = $i*2;
+            }
+            
+            for($i = 0; $i < 10; ++$i)
+            {
+                echo "$vector[$i]";
+            }
+
+            ?>
+        </div><br>
+
+    </body>
+</html>
