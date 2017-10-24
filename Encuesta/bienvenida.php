2b329c7529654d3efb5015c294a3759cdb1eecf0
UrielKaralden <urielkaralden@gmail.com>
2017-10-24 08:50:57 +0200

    Welcome page of the interview

---
+++ Encuesta/bienvenida.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title> Acceso a la encuesta </title>
+    </head>
+    <body>
+        <h1> Acceso a la encuesta</h1>
+        <div align = "left">
+            Bienvenido a la encuesta <br>
+            Para comenzar, haga click en Comenzar <br>
+            <form action = "encuesta.php" method = "POST">
+                <input type ="submit" value="Comenzar"><br>
+            </form>
+        </div>
+    </body>
+</html>
