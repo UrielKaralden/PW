16915bdf4a5096cb72e3bca161b317e096b91d90
UrielKaralden <urielkaralden@gmail.com>
2017-10-24 09:46:19 +0200

    Header of the survey

---
+++ Encuesta/encabezado.php
@@ -0,0 +1,11 @@
+<?php
+    echo" <div align = \"center\">
+            <h1> <font style =\"text-transform: uppercase;\">
+                Universidad de Cádiz
+            </h1>
+            <h2> <font style =\"text-transform: uppercase;\">
+                Encuesta de opinión de los/as estudiantes <br>
+                sobre la labor docente del profesorado
+            </h2>
+        </div>";
+?>
