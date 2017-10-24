e22db4f7af547718a2fcdd6c6b341d2d117918b3
UrielKaralden <urielkaralden@gmail.com>
2017-10-24 10:44:15 +0200

    Corrected errors of sql syntax

--- Encuesta/encuesta_profesorado_santi.sql
+++ Encuesta/encuesta_profesorado_santi.sql
@@ -1,28 +1,28 @@
-CREATE TABLE Estudios {
+CREATE TABLE Estudios (
   id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   nombre tinytext NOT NULL
-};
+);
 
-CREATE TABLE Encuestas {
+CREATE TABLE Encuestas (
   id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   id_Estudios int NOT NULL,
   FOREIGN KEY (id_Estudios) REFERENCES Estudios(id)
-};
+);
 
-CREATE TABLE Secciones {
+CREATE TABLE Secciones (
   id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-};
+);
 
-CREATE TABLE Preguntas {
+CREATE TABLE Preguntas (
   id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   id_Secciones int NOT NULL,
   FOREIGN KEY (id_Secciones) REFERENCES Secciones(id)
-};
+);
 
-CREATE TABLE Respuestas {
+CREATE TABLE Respuestas (
   id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   id_Preguntas int NOT NULL,
   id_Encuestas int NOT NULL,
   FOREIGN KEY (id_Preguntas) REFERENCES Dimensiones(id)
   FOREIGN KEY (id_EncuestasRellenas) REFERENCES Dimensiones(id)
-};
+);
