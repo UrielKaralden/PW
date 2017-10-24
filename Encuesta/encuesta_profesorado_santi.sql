d9dc5f252fa0a8089bae641082775a55623e4342
span-from-the-block <szlavalle@gmail.com>
2017-10-24 10:33:59 +0200

    Añadido diseño de BD

---
+++ Encuesta/encuesta_profesorado_santi.sql
@@ -0,0 +1,28 @@
+CREATE TABLE Estudios {
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nombre tinytext NOT NULL
+};
+
+CREATE TABLE Encuestas {
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id_Estudios int NOT NULL,
+  FOREIGN KEY (id_Estudios) REFERENCES Estudios(id)
+};
+
+CREATE TABLE Secciones {
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+};
+
+CREATE TABLE Preguntas {
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id_Secciones int NOT NULL,
+  FOREIGN KEY (id_Secciones) REFERENCES Secciones(id)
+};
+
+CREATE TABLE Respuestas {
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id_Preguntas int NOT NULL,
+  id_Encuestas int NOT NULL,
+  FOREIGN KEY (id_Preguntas) REFERENCES Dimensiones(id)
+  FOREIGN KEY (id_EncuestasRellenas) REFERENCES Dimensiones(id)
+};
