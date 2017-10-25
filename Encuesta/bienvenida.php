d16c8164767897f7dd6efdae136235eac65ef6a2
UrielKaralden <urielkaralden@gmail.com>
2017-10-25 19:58:50 +0200

    Changing alignment to center instead of left

--- Encuesta/bienvenida.php
+++ Encuesta/bienvenida.php
@@ -4,7 +4,7 @@
     </head>
     <body>
         <h1> Bienvenido al Sistema de Encuestas al Alumnado </h1>
-        <div align = "left">
+        <div align = "center">
             Para acceder al sistema, haga click en Comenzar <br>
             <form action = "seleccion_encuesta.php" method = "POST">
                 <input type ="submit" value="Comenzar"><br>
