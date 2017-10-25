f3499d563c4307831d977bb618e19c4b461209f4
UrielKaralden <urielkaralden@gmail.com>
2017-10-25 19:50:08 +0200

    Change welcome page to a new survey system

--- Encuesta/bienvenida.php
+++ Encuesta/bienvenida.php
@@ -1,13 +1,12 @@
 <html>
     <head>
-        <title> Acceso a la encuesta de satisfaccion </title>
+        <title> Bienvenida al sistema de encuestas </title>
     </head>
     <body>
-        <h1> Acceso a la encuesta</h1>
+        <h1> Bienvenido al Sistema de Encuestas al Alumnado </h1>
         <div align = "left">
-            Bienvenido a la encuesta <br>
-            Para comenzar, haga click en Comenzar <br>
-            <form action = "encuesta.php" method = "POST">
+            Para acceder al sistema, haga click en Comenzar <br>
+            <form action = "seleccion_encuesta.php" method = "POST">
                 <input type ="submit" value="Comenzar"><br>
             </form>
         </div>
