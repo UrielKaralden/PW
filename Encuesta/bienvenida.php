d73932f738cb719d9665bb6364d6b1c3a3377376
UrielKaralden <urielkaralden@gmail.com>
2017-10-27 15:02:12 +0200

    Centered title

--- Encuesta/bienvenida.php
+++ Encuesta/bienvenida.php
@@ -3,8 +3,8 @@
         <title> Bienvenida al sistema de encuestas </title>
     </head>
     <body>
-        <h1> Bienvenido al Sistema de Encuestas al Alumnado </h1>
         <div align = "center">
+            <h1> Bienvenido al Sistema de Encuestas al Alumnado </h1>
             Para acceder al sistema, haga click en Comenzar <br>
             <form action = "select_encuesta.php" method = "POST">
                 <input type ="submit" value="Comenzar"><br>
