92bc856b32c3782a47c43e6a28a99db28d4dc370
UrielKaralden <urielkaralden@gmail.com>
2017-10-31 10:03:49 +0100

    Go to Login.html

--- Encuesta/bienvenida.php
+++ Encuesta/bienvenida.php
@@ -6,7 +6,7 @@
         <div align = "center">
             <h1> Bienvenido al Sistema de Encuestas al Alumnado </h1>
             Para acceder al sistema, haga click en Comenzar <br>
-            <form action = "select_encuesta.php" method = "POST">
+            <form action = "Login.html" method = "POST">
                 <input type ="submit" value="Comenzar"><br>
             </form>
         </div>
