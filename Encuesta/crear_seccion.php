628aab58734d4a49d6df3e15f6f4d48da813a72f
LuisIM21 <RiBan2012@gmail.com>
2017-10-31 10:35:03 +0100

    Added crear_seccion.php

---
+++ Encuesta/crear_seccion.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<title>
+			Crear Seccion
+		</title>
+	</head>
+	<body>
+		<center>
+		<form method="POST" action="Modsec.php">
+			<input type="text" name="nsection" placeholder="Section name"/>
+			<br />
+			<input type="text" name="modcre" placeholder="Modificar o Crear"/>
+			<br />
+			<button type="submit">Continuar</button>
+		<form>
+		</center>
+	</body>	
+</html>
