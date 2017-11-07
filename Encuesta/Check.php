488da272967a4d9cb0eb7a2591bb83d31f9a31f7
UrielKaralden <urielkaralden@gmail.com>
2017-11-07 19:25:18 +0100

    Improved versions of these files

--- Encuesta/Check.php
+++ Encuesta/Check.php
@@ -7,39 +7,32 @@
 	endif;
 
 	$pswdHash = password_hash($pswd, PASSWORD_BCRYPT); // pswdHash = contraseña encriptada
-	
-	if(empty($usuario) && empty($pswd)) :
+	if(empty($usuario) && empty($pswd))
+	{
 		header("Location: Login2.html");
 		exit();
-	endif;
+	}
 
 	$varConn = mysqli_connect('localhost','root','','Encuesta_profesorado')
-			   or die ("Error al conectar " . mysqli_error());
+			   or die("Error al conectar " . mysqli_error());
 	
 	$query = "SELECT * FROM usuarios WHERE user = '$usuario'";
-	$result = mysqli_query($varConn, $query);
 
-	if($row = mysqli_fetch_array($result))
-	{
-		if($row['password']==$pswdHash)
-		{
+	$result = (mysqli_query($varConn, $query);
+
+	if($row=$mysqli_fecth_array($result)){
+		if($row['password']==$pswdHash){
 			session_start();
 			$_SESSION['usuario']=$usuario;
 			$_SESSION['admin']=$row['admin'];
 			$_SESSION['id_Estudio']=$row['id_Estudio'];
 			header("Location: select_encuesta.php");
-		}
-		else
-		{
-			echo "Fuera Invasor";
-			header("Location: Login2.html");
+		}else{
+			echo"<script>alert('Fuera Invasor');window.location='Login.html';</script>";
 			exit();
 		}
-	}
-	else
-	{
-		echo "Fuera Invasor";
-		header("Location: Login2.html");
+	}else{
+		echo"<script>alert('Fuera Invasor');window.location='Login.html';</script>";
 		exit();
 	}
 	/*mysqli_connect('localhost','root','','Encuesta_profesorado')or die("Error al conectar " . mysqli_error());
@@ -52,25 +45,12 @@
 			$_SESSION['admin']=true;
 			header("Location: select_encuesta.php");
 		}else{
-			echo "Fuera Invasor";
-			header("Location: Login2.html");
+			echo"<script>alert('Fuera Inavasor');window.location='Login.html';</script>";
 			exit();
 		}
 	}else{
-		echo "Fuera Invasor";
-		header("Location: Login2.html");
+		echo"<script>alert('Fuera Invasor');window.location='Login.html';</script>";
 		exit();
 	}*/
 
-/*
-SELECT * FROM Respuestas
-WHERE id_Encuesta = 10
-AND id_Preguntas IN
- (SELECT id FROM Preguntas
-  WHERE id_Secciones = 15);
-*/
-
 ?>
-
-
-
--- Encuesta/Modsec.php
+++ Encuesta/Modsec.php
@@ -1,6 +1,7 @@
 <?php
 	$nombre=$_POST['nsection'];
 	$action=$_POST['modcre'];
+	$identificator=$_POST['iden'];
 	if(empty($nombre) && empty($action)){
 		echo"<script>alert('Campos Vacios');window.location='crear_seccion.php';</script>";
 		exit();
@@ -8,16 +9,23 @@
 	else{
 		mysqli_connect('localhost','root','','Encuesta_profesorado')or die("Error al conectar " . mysqli_error());
 		if($action=="Modificar"){
-			$result=mysqli_query("SELECT * from seccion where nombre='" . nsection . "'");
+			$result=mysqli_query("SELECT * from seccion where nombre='$nombre' and id_Encuesta='$iden'");
 			if($row=$mysqli_fecth_array($result)){
-			
+				 echo "<form action = \"pregunta_encuesta.php\" method = \"POST\">
+				<input type = \"hidden\" name = \"id_section\" value = \"$row['id']\"><br>
+				</form>";
+				exit();
 			}else{
 				echo"<script>alert('No existe seccion');window.location='crear_seccion.php';</script>";
 				exit();
 			}
 		}else{
 			if($action=="Crear"){
-				echo"<script>alert('Exito al crear');window.location='crear_seccion.php';</script>";
+				$result = mysqli_query("INSERT INTO seccion(id,nombre,id_Encuesta) VALUES('', '$nombre','$identificator')");
+				if($result)
+					echo"<script>alert('Exito al crear');window.location='crear_seccion.php';</script>";
+				else
+					echo"<script>alert('Ha ocurrido un error, vuelva a intentarlo');window.location='crear_seccion.php';</script>";
 				exit();
 			}else{
 				echo"<script>alert('Esa opcion no existe');window.location='crear_seccion.php';</alert>";
