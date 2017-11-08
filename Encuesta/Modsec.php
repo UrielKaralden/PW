0929592f48f70706c4ff321eb7b92460817b6fdc
span-from-the-block <szlavalle@gmail.com>
2017-11-08 11:11:25 +0100

    ...

--- Encuesta/Modsec.php
+++ Encuesta/Modsec.php
@@ -1,25 +1,35 @@
 <?php
 	$nombre=$_POST['nsection'];
 	$action=$_POST['modcre'];
-	if(empty($nombre) && empty($action)){
+	if(empty($nombre) && empty($action))
+	{
 		echo"<script>alert('Campos Vacios');window.location='crear_seccion.php';</script>";
 		exit();
 	}
-	else{
+	else
+	{
 		mysqli_connect('localhost','root','','Encuesta_profesorado')or die("Error al conectar " . mysqli_error());
-		if($action=="Modificar"){
+		if($action=="Modificar")
+		{
 			$result=mysqli_query("SELECT * from seccion where nombre='" . nsection . "'");
-			if($row=$mysqli_fecth_array($result)){
-			
-			}else{
+			if($row=$mysqli_fecth_array($result))
+			{	
+			}
+			else
+			{
 				echo"<script>alert('No existe seccion');window.location='crear_seccion.php';</script>";
 				exit();
 			}
-		}else{
-			if($action=="Crear"){
+		}
+		else
+		{
+			if($action=="Crear")
+			{
 				echo"<script>alert('Exito al crear');window.location='crear_seccion.php';</script>";
 				exit();
-			}else{
+			}
+			else
+			{
 				echo"<script>alert('Esa opcion no existe');window.location='crear_seccion.php';</alert>";
 				exit();
 			}
--- Encuesta/check_usuario.php
+++ Encuesta/check_usuario.php
@@ -32,36 +32,16 @@
 		else
 		{
 			echo "Fuera Invasor";
-			header("Location: Login2.html");
+			header("Location: Login3.html");
 			exit();
 		}
 	}
 	else
 	{
 		echo "Fuera Invasor";
-		header("Location: Login2.html");
+		header("Location: Login3.html");
 		exit();
 	}
-	/*mysqli_connect('localhost','root','','Encuesta_profesorado')or die("Error al conectar " . mysqli_error());
-
-	$result=mysql_query("SELECT * from admin where user='" . $usuario . "'");
-	if($row=$mysql_fecth_array($result)){
-		if($row['password']==$pswd){
-			session_start();
-			$_SESSION['usuario']=$usuario;
-			$_SESSION['admin']=true;
-			header("Location: select_encuesta.php");
-		}else{
-			echo "Fuera Invasor";
-			header("Location: Login2.html");
-			exit();
-		}
-	}else{
-		echo "Fuera Invasor";
-		header("Location: Login2.html");
-		exit();
-	}*/
-
 /*
 SELECT * FROM Respuestas
 WHERE id_Encuesta = 10
