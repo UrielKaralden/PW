4d3990e19bbf4d3204143a7060e13a7b1bf85558
UrielKaralden <urielkaralden@gmail.com>
2017-11-09 10:09:05 +0100

    First version for survey view

--- Encuesta/encuesta.php
+++ Encuesta/encuesta.php
@@ -17,14 +17,15 @@
 						$secciones_encuesta = mysqli_query($conexion, $secciones_query);
 						$preguntas_query = "Select * from preguntas where id_Secciones = $secciones_encuesta order by id,id_Secciones asc;";
 						$preguntas = mysqli_query($conexion, $preguntas_query);
-						$num_preguntas =
 
 						echo "<table border = 1>";
 		                echo "<div align=center>\n";
 
+						//mysqli_fetch_array
+						//mysqli_fetch_field Próxima tupla
 						//Sustituir bucle por fetch
-						for($iter_bucle = 0; $iter_bucle < $num_preguntas; ++$iter_bucle)
-		                {
+						while($next_question = mysqli_fetch_field($preguntas))
+						{
 		                  // 1º Cambiamos el color del fondo de la tabla en funcion
 		                  // de la fila de la tabla
 		                	if($iter_bucle%2==0)
@@ -32,12 +33,28 @@
 		                	else
 		                      echo "<tr>\n";
 
-		                	$tipo_pregunta_query = "Select tipo_pregunta from preguntas where id = $next_id;";
-							$tipo_pregunta = mysqli_query($conexion, $tipo_pregunta_query);
-							$pregunta_query = "Select pregunta from preguntas where id = $next_id;";
-							echo ""
-		                }
+							$num = $next_question->id;
+							$section = $next_question->id_Secciones;
+							$question_type = $next_question->tipo;
+							$text = $next_question->pregunta;
 
+							echo "$texto<br>";
+							if ($question_type == 'radio')
+							{
+								$respuestas_query = "Select * from radio_respuestas where id_Pregunta = $num";
+								$respuestas = mysqli_query($conexion, $respuestas_query);
+								while($info = mysqli_fecth_field($respuestas))
+								{
+									echo "<input type = \"$question_type\" name = \"$num\" value = \"$info->texto\"><br>";
+								}
+							}
+							else
+							{
+								echo "<input type = \"$question_type\" name = \"$num\" placeholder="Responda aqui"/><br><br>"
+							}
+
+		                }
+						echo "<button type=\"submit\">Enviar respuestas</button>";
                     ?>
 				</form>
 			</center>
