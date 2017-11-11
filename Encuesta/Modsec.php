320e74ffdfe8b88da2e17e62cbcd7c307158937d
UrielKaralden <urielkaralden@gmail.com>
2017-11-12 00:56:22 +0100

    Changed option Eliminar

--- Encuesta/Modsec.php
+++ Encuesta/Modsec.php
@@ -35,29 +35,105 @@
 				exit();
 			}else{
 				if($action=="Eliminar"){
-					$sections_id_query = "SELECT id from secciones where id_Encuesta='identificator';";
-					$sections_ids = mysqli_query($conexion, $sections_id_query);
-					$row = mysqli_fetch_assoc($sections_ids)
-					while($section_field = mysqli_fetch_field($row))
+					$section_id_query = "SELECT id from secciones where nombre='$nombre';";
+					$section_id = mysqli_query($conexion, $section_id_query);
+
+					$preguntas_query = "SELECT * FROM preguntas WHERE id_Secciones = '$section_id';";
+					$preguntas_object = mysqli_query($conexion, $preguntas_query);
+					$preguntas = mysqli_fetch_assoc($preguntas_object);
+					while($row = mysqli_fetch_field($preguntas))
 					{
-						$identificador=$row['id'];
-						for($iter_bucle = 0; $iter_bucle < $num_encuestas; ++$iter_bucle)
+						$pregunta_id = $row['id'];
+						$pregunta_type = $row['tipo_pregunta'];
+						if($pregunta_type == 'radio')
 						{
-							$result=mysqli_query("SELECT * from preguntas where id='$iter_bucle' AND id_Seccion='$identificador'";);
-							if($row2=$mysqli_fetch_array($result))
-							echo"<form method=\"POST\" action=\"ModPreg.php\">
-							<input type=\"hidden\" name=\"nquestion\" value=$row2['nombre']/>
-							<input type=\"hidden\" name=\"typequestion\" value=$row2['tipo_pregunta']/>
-							<input type=\"hidden\" name=\"modcre\" value=\"Eliminar\"/>
-							<input type=\"hidden\" name=\"iden\" value=$identificador />";
+							// Eliminamos las radio_respuestas de esa pregunta
+							$delete_radio_respuestas_query = "DELETE * FROM radio_respuestas where id_Pregunta = $pregunta_id;";
+							$delete_radio_respuestas = mysqli_query($conexion, $delete_radio_respuestas_query);
+							if($delete_radio_respuestas)
+								echo "RADIO RESPUESTAS ELIMINADAS CORRECTAMENTE";
+							else
+							{
+								echo "MERECES LA MUERTE, PINGU";
+							}
+
+							//Actualizamos los IDs de las demas radio_respuestas
+							$select_ids_radio_respuestas_query = "SELECT id FROM radio_respuestas ORDER BY id asc;";
+							$select_ids_radio_respuestas = mysqli_query($conexion, $select_ids_radio_respuestas_query);
+							$radio_respuestas_ids = mysqli_fetch_assoc($select_ids_radio_respuestas);
+							$cont = 1;
+							while($rr_row = mysqli_fetch_field($radio_respuestas_ids))
+							{
+								$old_id = $rr_row['id'];
+								$update_rr_query = "UPDATE radio_respuestas SET id = '$cont' where radio_respuestas.id = '$old_id';";
+								$update_rr = mysqli_query($conexion, $update_rr_query);
+								if($update_rr)
+									echo "RADIO RESPUESTAS ACTUALIZADAS CORRECTAMENTE";
+								else
+								{
+									echo "MERECES LA MUERTE, YASUO";
+								}
+							}
 						}
-						$result=mysqli_query("DELETE FROM secciones where id='$identificador'");
-						if($result)
-							echo"<script>alert('Exito al eliminar');window.location='crear_seccion.php';</script>";
+						// Eliminamos las respuestas de esa pregunta
+						$delete_respuestas_query = "DELETE * FROM respuestas WHERE id_Preguntas = $pregunta_id;";
+						$delete_respuestas = mysqli_query($conexion, $delete_respuestas_query);
+						if($delete_respuestas)
+							echo "RESPUESTAS ELIMINADAS CORRECTAMENTE";
 						else
-							echo"<script>alert('Ha ocurrido un error, vuelva a intentarlo');window.location='crear_seccion.php';</script>";
-						exit();
-					}else{
+						{
+							echo "MERECES LA MUERTE, RONALD MACDONALD";
+						}
+
+						// Actualizar los ids de respuestas
+						$select_ids_respuestas_query = "SELECT id FROM respuestas ORDER BY id asc;";
+						$select_ids_respuestas = mysqli_query($conexion, $select_ids_respuestas_query);
+						$respuestas_ids = mysqli_fetch_assoc($select_ids_respuestas);
+						$cont = 1;
+						while($r_row = mysqli_fetch_field($radio_respuestas_ids))
+						{
+							$r_old_id = $r_row['id'];
+							$update_r_query = "UPDATE respuestas SET id = '$cont' where respuestas.id = '$r_old_id';";
+							$update_respuestas = mysqli_query($conexion, $update_r_query);
+							if($update_respuestas)
+								echo "RESPUESTA ACTUALIZADAS CORRECTAMENTE";
+							else
+							{
+								echo "MERECES LA MUERTE, LLOQUER";
+							}
+						}
+
+						// Eliminamos la pregunta en cuestion
+						$delete_pregunta_query = "DELETE * FROM preguntas WHERE id = $pregunta_id;";
+						$delete_pregunta = mysqli_query($conexion, $delete_pregunta);
+						if($delete_pregunta)
+						{
+							echo "PREGUNTA ELIMINADA CORRECTAMENTE";
+						}
+						else
+						{
+							echo "MERECES LA MUERTE, GRIM MATCHSTICK";
+						}
+					}
+					// Actualizamos las IDs de las preguntas
+					$select_ids_preguntas_query = "SELECT id FROM preguntas ORDER BY id asc;";
+					$select_ids_preguntas = mysqli_query($conexion, $select_ids_preguntas_query);
+					$preguntas_ids = mysqli_fetch_assoc($select_ids_preguntas);
+					$cont = 1;
+					while($p_row = mysqli_fetch_field($preguntas_ids))
+					{
+						$p_old_id = $p_row['id'];
+						$update_p_query = "UPDATE preguntas SET id = '$cont' where preguntas.id = '$p_old_id';";
+						$update_preguntas = mysqli_query($conexion, $update_p_query);
+						if($update_preguntas)
+							echo "PREGUNTAS ACTUALIZADAS CORRECTAMENTE";
+						else
+						{
+							echo "MERECES LA MUERTE, WEBMASTER";
+						}
+					}
+					else
+					{
 						echo"<script>alert('Ha ocurrido un error, vuelva a intentarlo');window.location='crear_seccion.php';</alert>";
 						exit();
 					}
