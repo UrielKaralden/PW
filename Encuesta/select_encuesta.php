71e29fc2a7fab2c52b13872230bb676f06c17ac9
UrielKaralden <urielkaralden@gmail.com>
2017-11-11 01:53:11 +0100

    Valid select_encuesta

--- Encuesta/select_encuesta.php
+++ Encuesta/select_encuesta.php
@@ -9,6 +9,7 @@
         </div>
         <div align = "center">
             <?php
+                define("ADMIN", 1);
                 error_reporting(E_ALL & ~E_NOTICE);
                 // Sessión con Mysql
                 // $user_estudio = Estudio al que pertenece el usuario
@@ -18,18 +19,23 @@
                 $user = mysqli_real_escape_string($conexion, $_SESSION['usuario']);
                 $user_admin = mysqli_real_escape_string($conexion, $_SESSION['admin']);
 
-                $buscar_estudio = "SELECT id_Estudio FROM Usuarios WHERE nombre = '$user';";
-                $estudio = mysqli_query($conexion, $buscar_estudio);
-                $buscar_encuestas = "SELECT id FROM Encuestas WHERE id_Estudio = '$user_estudio';";
+                $buscar_estudio = "SELECT * FROM Usuarios WHERE nombre = '$user';";
+                $estudio_res = mysqli_query($conexion, $buscar_estudio);
+                $estudio = mysqli_fetch_assoc($estudio_res);
+                $user_estudio = $estudio['id_Estudio'];
+                $buscar_encuestas = "SELECT * FROM Encuestas WHERE id_Estudio = '$user_estudio';";
                 $muestra_consultas = mysqli_query($conexion, $buscar_encuestas);
                 $num_encuestas = mysqli_num_rows($muestra_consultas);
 
                 echo "<table border = 1>";
-                echo "<div align = right>
-                      <form action = \"crear_encuesta.php\" method = \"POST\">
-                      <button type = \"submit\ value = \"Crear encuesta\">
-                      </form>
-                      </div>";
+                if($user_admin == ADMIN)
+                {
+                    echo "<div align = center>
+                        <form action = \"crear_encuesta.php\" method = \"POST\">
+                        <input type = \"submit\" value = \"Crear Encuesta\">
+                        </form>
+                        </div>";
+                }
                 echo "<div align=center>\n";
                 // Comprobar usuario
 
@@ -42,21 +48,27 @@
 
                 //Iniciar formulario
                 echo "<form action = \"$ref_encuesta\" method = \"POST\">";
-                for($iter_bucle = 0; $iter_bucle < $num_encuestas; ++$iter_bucle)
-                {
-                  // 1º Cambiamos el color del fondo de la tabla en funcion
-                  // de la fila de la tabla
-                  if($iter_bucle%2==0)
-                      echo "<tr bgcolor=#bdc3d6>\n";
-                  else
-                      echo "<tr>\n";
 
-                  echo "<input type = \"radio\" name = \"id_Encuesta\" value=\"$iter_bucle\"";
+                $cont = 0;
+                while($iter_bucle = mysqli_fetch_assoc($muestra_consultas))
+                {
+                    // 1º Cambiamos el color del fondo de la tabla en funcion
+                    // de la fila de la tabla
+                    if($cont%2==0)
+                        echo "<tr bgcolor=#bdc3d6>\n";
+                    else
+                        echo "<tr>\n";
+
+                    ++$cont;
+                    $nombre_encuesta = $iter_bucle['nombre'];
+                    echo "$nombre_encuesta <input type = \"radio\" name = \"id_Encuesta[]\" value=\"$iter_bucle\">";
+                    echo "<input type = \"submit\" value = \"Seleccionar\">";
                 }
-                echo "<input type = \"submit\" value = \"Seleccionar\">";
+                /*
                 if($user_admin == 1)
-                    echo "<button type = \"submit\" value = \"Administrar\";";
+                    //echo "<button type = \"submit\" value = \"Administrar\";";
                 echo "</form>";
+                //echo "$user_admin";*/
             ?>
         </div>
     </body>
